b096cf74e577c5cc56d04b93bc684dc4a3c7de0a
dori <dori.claudino@gmail.com>
2020-11-13 23:12:27 -0500

    v.1 no with some tests

--- src/index.js
+++ src/index.js
@@ -1,13 +1,52 @@
 import "./index.css";
 
-class MyLibrary {
-  constructor() {
-    console.log("Library constructor loaded");
-  }
+let replacesRegex = {
+  keepFractionSpaces: {
+    regex: /(\s+)(?!(\d\⁄))/gim,
+    subst: ``,
+  },
+  feetAndInchesAndFraction: {
+    regex: /(\-*?\d+\s*?)ft\s*?(\d+\s*?(?!\d+\⁄))\s*?(\d+?)\⁄(\d+?)/gim,
+    subst: `(($1*12) + ($2 + $3/$4))`,
+  },
+  feetAndInches: {
+    regex: /(\-*?\d+\s*?)ft\s*?(\d+)/gim,
+    subst: `(($1*12) + $2)`,
+  },
+  feetAndFractions: {
+    regex: /(\-*?\d+\s*?)ft\s*?(\d+)\⁄(\d+)/gim,
+    subst: `(($1*12) + $2/$3)`,
+  },
+  feet: {
+    regex: /(\-*?\d+)\s*?ft/gim,
+    subst: `($1 * 12)`,
+  },
+  inchesAndFractions: {
+    regex: /(\-*?\d+\s*?)\s*?(\d+)\⁄(\d+)/gim,
+    subst: `($1 + $2/$3)`,
+  },
+  fractions: {
+    regex: /(\d+)\⁄(\d+)/gim,
+    subst: `($1/$2)`,
+  },
+  divide: {
+    regex: /÷/gim,
+    subst: `/`,
+  },
+  multiply: {
+    regex: /×/gim,
+    subst: `*`,
+  },
+};
 
-  myMethod = () => {
-    console.log("Library method fired");
-  };
+function parser(expression = "") {
+  let temp = expression;
+
+  Object.values(replacesRegex).forEach(({ regex, subst }) => {
+    temp = temp.replace(regex, subst);
+  });
+  const result = eval(temp);
+  return result;
 }
 
-export default MyLibrary;
+export default parser;
--- tests/test.js
+++ tests/test.js
@@ -1,5 +1,32 @@
-import MyLibrary from '../src/index';
+import parser from "../src/index";
 
-test('Runs without crashing', () => {
-  new MyLibrary();
+test("Runs without crashing", () => {
+  parser();
+});
+
+test("eval is work", () => {
+  expect(parser("1*3")).toBe(3);
+  expect(parser("1*3+2")).toBe(5);
+  expect(parser("5/5+4*2")).toBe(9);
+});
+
+test("base sum", () => {
+  expect(parser("1+3+2")).toBe(6);
+  expect(parser("1+3+2+5")).toBe(11);
+  expect(parser("1 + 3+  5")).toBe(9);
+});
+
+test("decimals and fractions together", () => {
+  expect(parser(`1⁄2 + 1 + 0.66`)).toBe(2.16);
+});
+
+test("check result", () => {
+  expect(parser(`1⁄2 + 1`)).toBe(1.5);
+  expect(parser(`(2 1⁄4)÷(2 1⁄2)`)).toBe(0.9);
+  expect(parser(`2 1⁄4÷2 1⁄2`)).toBe(0.9);
+  expect(parser(`3ft+ 2 1⁄4÷2 1⁄2`)).toBe(36.9);
+  expect(parser(`-5ft*2`)).toBe(-120);
+  expect(parser(`-5ft*2+10`)).toBe(-110);
+  expect(parser(`-5ft*2+3ft`)).toBe(-84);
+  expect(parser(`-5ft*2+3ft+4 1⁄2`)).toBe(-79.5);
 });
